d9e20cfd91b7ca8422c2dc86b92fc572acdfadd9
lizasiti <sitinorhaliza289@gmail.com>
2025-01-11 21:10:38 +0700

    first commit

---
+++ resources/views/client/components/molecules/hero/text-block.blade.php
@@ -0,0 +1,8 @@
+@push('css')
+    <link rel="stylesheet" href="{{ asset('client/components/molecules/hero/text-block.css') }}">
+@endpush
+
+<div class="text-center text-block-hero py-5">
+    <h1>Sustainable yet stylish lifestye</h1>
+    <p>Discover the best fashion collections at (LSTORE), an online store offering a wide range of high-quality products such as modern kebaya, couple kebaya, women’s bags, and elegant heels to complement your style on every special occasion. With designs that blend traditional elegance and modern touches, we provide options that are comfortable, stylish, and perfect for any event. Enjoy a seamless shopping experience, affordable prices, and excellent service only at (LSTORE). Look confident and memorable with our exclusive collections!</p>
+</div>
